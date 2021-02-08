dee69487fd17115404f540ddc7fe8b0f7d7f1d7d
emnh <hvidevold@gmail.com>
2021-02-08 14:42:59 +0100

    1 file modified

--- index.js
+++ index.js
@@ -14,6 +14,101 @@ $("canvas").css("position", "absolute");
 $("canvas").css("left", "140px");
 $("canvas").css("top", "120px");
 
+class Belt {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = 0xff0000;
+  }
+}
+
+const fragment = `
+  varying vec2 vTextureCoord;
+  uniform sampler2D uSampler;
+  uniform float time;
+  
+  void main(){
+    vec2 uv = vec2(vTextureCoord.x, 1.-vTextureCoord.y);
+    uv.x = mod(uv.x + time, 1.0);
+    gl_FragColor = texture2D(uSampler, uv);
+  }
+`;
+
+const filters = [];
+
+class Conveyor {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.color = 0xff0000;
+  }
+
+  draw() {
+    //const renderer = PIXI.autoDetectRenderer(this.width, this.height);
+
+    const container = new PIXI.Container();
+    const graphics = new PIXI.Graphics();
+
+    graphics.lineStyle(1, this.color, 1);
+    const bands = 3;
+    for (let i = 0; i < bands; i++) {
+      const xoffset = (i * 0.5 * (this.width - 1)) / bands + 1;
+      const dheight = 0.1;
+      graphics.moveTo(xoffset, this.height * 0.5);
+      graphics.lineTo(this.width * 0.5 + xoffset, this.height * dheight);
+      graphics.moveTo(xoffset, this.height * 0.5);
+      graphics.lineTo(
+        this.width * 0.5 + xoffset,
+        this.height * (1.0 - dheight)
+      );
+    }
+    graphics.position.x = 0.5 * this.width;
+    graphics.position.y = 0.5 * this.height;
+    graphics.pivot.set(0.5 * this.width, 0.5 * this.height);
+    //graphics.rotation = 0.5 * Math.PI * Math.floor(Math.random() * 4);
+    container.width = this.width;
+    container.height = this.height;
+
+    const filter = new PIXI.Filter(null, fragment, {
+      time: 0.0
+    });
+
+    //Apply it to our object
+    //graphics.filters = [filter];
+
+    //graphics.endFill();
+    container.addChild(graphics);
+
+    const sprites = [];
+    const spriteCount = 10;
+    for (let i = 0; i < spriteCount; i++) {
+      const renderTexture = PIXI.RenderTexture.create({
+        width: this.width,
+        height: this.height
+      });
+      filter.uniforms.time = i / spriteCount;
+      app.renderer.render(container, renderTexture);
+      //console.log(app.renderer);
+      //renderTexture.updateUvs();
+      renderTexture.updateUvs();
+      sprites.push(renderTexture);
+      //sprites[sprites.length - 1].texture.updateUVs();
+    }
+    //console.log(sprites);
+
+    const anim = new PIXI.AnimatedSprite(sprites);
+    anim.animationSpeed = 1.0;
+    anim.play();
+    return anim;
+  }
+}
+
+const conveyor = new Conveyor(100, 100).draw();
+
+app.stage.addChild(conveyor);
+
 class Cell {
   constructor(x, y, width, height, obj) {
     this.x = x;
@@ -25,11 +120,13 @@ class Cell {
   }
 
   draw() {
+    const container = new PIXI.Container();
     const graphics = new PIXI.Graphics();
-    graphics.beginFill(this.color);
-    graphics.drawCircle(0, 0, this.width);
-    graphics.endFill();
-    return graphics;
+    return container;
+  }
+
+  animate() {
+    this.graphics.x += 1.0;
   }
 }
 
@@ -52,8 +149,8 @@ class Grid {
 
   draw() {
     const container = new PIXI.Container();
-
     const graphics = new PIXI.Graphics();
+    container.addChild(graphics);
 
     //graphics.beginFill(this.color);
     graphics.lineStyle(2, this.gridColor, 1);
@@ -74,14 +171,31 @@ class Grid {
         cell.graphics.x = x * cell.width;
         cell.graphics.y = y * cell.height;
         container.addChild(cell.graphics);
-        console.log(x, y);
+        //console.log(x, y);
       }
     }
-    container.addChild(graphics);
 
-    return graphics;
+    return container;
+  }
+
+  animate() {
+    for (let x = 0; x < this.xdim; x++) {
+      for (let y = 0; y < this.ydim; y++) {
+        const cell = this.grid[x][y];
+        cell.animate();
+      }
+    }
   }
 }
 
 let grid = new Grid(width, height, width / 30, height / 30);
 app.stage.addChild(grid.draw());
+
+const raf = () => {
+  //grid.animate();
+  // for (let i = 0; i < filters.length; i++) {
+  //   filters[i].uniforms.time = performance.now() / 1000.0;
+  // }
+  requestAnimationFrame(raf);
+};
+requestAnimationFrame(raf);
